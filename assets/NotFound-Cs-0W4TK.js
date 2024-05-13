8a09656a88e6e5a5ed1211e0d094b3069aad6df7
Ruzel Yusupov <yezzeria@gmail.com>
2024-05-13 13:27:42 +0300

    deploy

---
+++ assets/NotFound-Cs-0W4TK.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-C7wZZfw7.js";const n={};function t(r,s){return o(),c("div",null,"Страница не найдена")}const a=e(n,[["render",t]]);export{a as default};
--- assets/NotFound-DIDXcWRY.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-Dl1hoCnj.js";const n={};function t(r,s){return o(),c("div",null,"Страница не найдена")}const a=e(n,[["render",t]]);export{a as default};
