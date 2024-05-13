15f5058ade44d15b8353fa8cad01f51f78464fe8
Ruzel Yusupov <yezzeria@gmail.com>
2024-05-13 11:24:13 +0300

    fix

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# остановить публикацию при ошибках
+set -e
+
+# сборка
+npm run build
+
+# переход в каталог сборки
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# если вы публикуете по адресу https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/yezzeria/js-pet-project.git main:gh-pages
+
+cd -
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import MainLayout from "../layouts/MainLayout.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), //import.meta.env.BASE_URL
+  history: createWebHashHistory(), //import.meta.env.BASE_URL
   routes: [
     {
       path: "/",
